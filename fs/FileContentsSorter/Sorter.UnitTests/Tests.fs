b2d57bfb30f943fbf3ac3cad56c19c7ebd5e36e4
nikolay.mozgovoy <nikolay.mozgovoy@sigma.software>
2018-10-22 14:44:32 +0300

    fs 2 merge functions left, lazy is still slow

--- fs/FileContentsSorter/Sorter.UnitTests/Tests.fs
+++ fs/FileContentsSorter/Sorter.UnitTests/Tests.fs
@@ -5,8 +5,8 @@ open Xunit
 open Core
 
 [<Fact>]
-let ``merge2 for 2 seq returns single ordered seq`` () =
-    let mergedSequence = merge2 [1 ; 4 ; 5] [2 ; 4; 7]
+let ``mergeSeq for 2 seq returns single ordered seq`` () =
+    let mergedSequence = mergeSeq [1 ; 4 ; 5] [2 ; 4; 7]
     let expectedSequence = seq {
         yield 1 
         yield 2
@@ -18,11 +18,30 @@ let ``merge2 for 2 seq returns single ordered seq`` () =
     Assert.Equal<IEnumerable<int>>(expectedSequence, mergedSequence)
 
 [<Fact>]
-let ``quasi performance test for merge2`` () =
-    let len = 100
-    let s1 = seq { for n in [1..len] do yield n }
-    let s2 = seq { for n in [1..len] do yield n + n }
+let ``mergeLists for 2 seq returns single ordered list`` () =
+    let mergedSequence = mergeLists [1 ; 4 ; 5] [2 ; 4; 7]
+    let expectedSequence = seq {
+        yield 1 
+        yield 2
+        yield 4
+        yield 4
+        yield 5
+        yield 7
+    }
+    Assert.Equal<IEnumerable<int>>(expectedSequence, mergedSequence)
 
-    let mergedSequence = merge2 s1 s2
+//--------------------------------------------------------------------
 
+let len = 100
+let s1 = seq { for n in [1..len] do yield n } |> Seq.toList
+let s2 = seq { for n in [1..len] do yield n + 1 } |> Seq.toList
+
+[<Fact>]
+let ``quasi performance test for mergeLists`` () =
+    let mergedSequence = mergeLists s1 s2
+    Assert.Equal(len * 2, mergedSequence |> Seq.length)
+
+[<Fact>]
+let ``quasi performance test for mergeSeq`` () =
+    let mergedSequence = mergeSeq s1 s2
     Assert.Equal(len * 2, mergedSequence |> Seq.length)
--- fs/FileContentsSorter/Sorter/Core.fs
+++ fs/FileContentsSorter/Sorter/Core.fs
@@ -1,21 +1,30 @@
 ﻿module Core
 
-let rec merge2 (seqA:seq<'t>) (seqB:seq<'t>) :seq<'t> =
+let mergeLists seqA seqB =
+    let rec mergeCont cont seqA seqB = 
+        match seqA, seqB with
+        | seqA, [] -> cont seqA
+        | [], seqB -> cont seqB
+        | hl::tl, hr::tr ->
+            if hl < hr then mergeCont (fun acc -> cont(hl::acc)) tl seqB
+            else mergeCont (fun acc -> cont(hr::acc)) seqA tr
+    mergeCont (fun x -> x) seqA seqB
+
+let rec mergeSeq (seqA:seq<'t>) (seqB:seq<'t>) :seq<'t> =
     let a = seqA |> Seq.tryHead
     let b = seqB |> Seq.tryHead
-    match (a, b) with
-        | (None, Some b) -> next b seqA (seqB |> Seq.tail)
-        | (Some a, None) -> next a (seqA |> Seq.tail) seqB
-        | (Some a, Some b) ->
-            let m = min a b
-            if m = a
-            then next a (seqA |> Seq.tail) seqB
-            else next b seqA (seqB |> Seq.tail)
-        | (None, None) -> Seq.empty
-
-and next value seqA seqB =
     seq {
-        yield value
-        for v in merge2 seqA seqB do
-        yield v
+        if a <> None && b <> None
+        then
+            if min a b <> b
+            then
+                yield Option.get a
+                yield! mergeSeq (Seq.skip 1 seqA) seqB
+            else
+                yield Option.get b
+                yield! mergeSeq seqA (Seq.skip 1 seqB)
+         elif a <> None
+         then yield! seqA
+         elif b <> None
+         then yield! seqB
     }
