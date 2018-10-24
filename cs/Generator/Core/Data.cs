fa883127a193e7652268eb32de25acb1a08fc236
Nick Brain <NicklausBrain@hotmail.com>
2018-10-24 23:21:26 +0300

    various poetry

--- cs/Generator/Core/Data.cs
+++ cs/Generator/Core/Data.cs
@@ -9,21 +9,168 @@ Whose seal is that? I think I know.
 Its owner is quite happy though.
 Full of joy like a vivid rainbow,
 I watch him laugh. I cry hello.
-
 He gives his seal a shake,
 And laughs until her belly aches.
 The only other sound's the break,
 Of distant waves and birds awake.
-
 The seal is acid, empty and deep,
 But he has promises to keep,
 After cake and lots of sleep.
 Sweet dreams come to him cheap.
-
 He rises from his gentle bed,
 With thoughts of kittens in his head,
 He eats his jam with lots of bread.
-Ready for the day ahead.";
+Ready for the day ahead.
+Roses are red,
+Violets are blue,
+A finger is little,
+And so are you.
+Orchids are white,
+Ghost ones are rare,
+Shadows are purple,
+And so is your hair.
+Magnolia grows,
+With buds like eggs,
+Means are powerful,
+And so are your legs.
+Sunflowers reach,
+Up to the skies,
+Products are toxic,
+And so are your eyes.
+Foxgloves in hedges,
+Surround the farms,
+Leukemia is hairy,
+And so are your arms.
+Daisies are pretty,
+Daffies have style,
+The idea is crazy,
+And so is your smile.
+A pony is beautiful,
+Just like you.
+Once upon a time there was a crazy girl called Rainbow Dash. She was on the way to see her pleasant Twilight Sparkle, when she decided to take a short cut through Hampstead Heath.
+It wasn't long before Rainbow got lost. She looked around, but all she could see were trees. Nervously, she felt into her bag for her favourite toy, Bunny, but Bunny was nowhere to be found! Rainbow began to panic. She felt sure she had packed Bunny. To make matters worse, she was starting to feel hungry.
+Unexpectedly, she saw a kind pony dressed in a purple waistcoat disappearing into the trees.
+""How odd!"" thought Rainbow.
+For the want of anything better to do, she decided to follow the peculiarly dressed pony. Perhaps it could tell him the way out of the forest.
+Eventually, Rainbow reached a clearing. She found herself surrounded by houses made from different sorts of food. There was a house made from carrots, a house made from macarons, a house made from jelly babies, a house made from chips, a house made from crisps and a house made from macarons.
+Rainbow could feel her tummy rumbling. Looking at the houses did nothing to ease her hunger.
+""Hello!"" she called. ""Is anybody there?""
+Nobody replied.
+Rainbow looked at the roof on the closest house and wondered if it would be rude to eat somebody else's chimney. Obviously it would be impolite to eat a whole house, but perhaps it would be considered acceptable to nibble the odd fixture or lick the odd fitting, in a time of need.
+A cackle broke through the air, giving Rainbow a fright. A witch jumped into the space in front of the houses. She was carrying a cage. In that cage was Bunny!
+""Bunny!"" shouted Rainbow. She turned to the witch. ""That's my toy!""
+The witch just shrugged.
+""Give Bunny back!"" cried Rainbow.
+""Not on your nelly!"" said the witch.
+""At least let Bunny out of that cage!""
+Before she could reply, five kind ponies rushed in from a footpath on the other side of the clearing. Rainbow recognised the one in the purple waistcoat that she'd seen earlier. The witch seemed to recognise him too.
+""Hello Big Pony,"" said the witch.
+""Good morning."" The pony noticed Bunny. ""Who is this?""
+""That's Bunny,"" explained the witch.
+""Ooh! Bunny would look lovely in my house. Give it to me!"" demanded the pony.
+The witch shook her head. ""Bunny is staying with me.""
+""Um... Excuse me..."" Rainbow interrupted. ""Bunny lives with me! And not in a cage!""
+Big Pony ignored her. ""Is there nothing you'll trade?"" he asked the witch.
+The witch thought for a moment, then said, ""I do like to be entertained. I'll release him to anybody who can eat a whole front door.""
+Big Pony looked at the house made from macarons and said, ""No problem, I could eat an entire house made from macarons if I wanted to.""
+""That's nothing,"" said the next pony. ""I could eat two houses.""
+""There's no need to show off,"" said the witch. Just eat one front door and I'll let you have Bunny.""
+Rainbow watched, feeling very worried. She didn't want the witch to give Bunny to Big Pony. She didn't think Bunny would like living with a kind pony, away from her house and all her other toys.
+The other four ponies watched while Big Pony put on his bib and withdrew a knife and fork from his pocket.
+""I'll eat this whole house,"" said Big Pony. ""Just you watch!""
+Big Pony pulled off a corner of the front door of the house made from macarons. He gulped it down smiling, and went back for more.
+And more. And more.
+Eventually, Big Pony started to get bigger - just a little bit bigger at first. But after a few more fork-fulls of macarons, he grew to the size of a large snowball - and he was every bit as round.
+""Erm... I don't feel too good,"" said Big Pony.
+Suddenly, he started to roll. He'd grown so round that he could no longer balance!
+""Help!"" he cried, as he rolled off down a slope into the forest.
+Big Pony never finished eating the front door made from macarons and Bunny remained trapped in the witch's cage.
+Average Pony stepped up, and approached the house made from jelly babies.
+""I'll eat this whole house,"" said Average Pony. ""Just you watch!""
+Average Pony pulled off a corner of the front door of the house made from jelly babies. She gulped it down smiling, and went back for more.
+And more.
+And more.
+After a while, Average Pony started to look a little queasy. She grew greener...
+...and greener.
+A woodcutter walked into the clearing. ""What's this bush doing here?"" he asked.
+""I'm not a bush, I'm a pony!"" said Average Pony.
+""It talks!"" exclaimed the woodcutter. ""Those talking bushes are the worst kind. I'd better take it away before somebody gets hurt.""
+""No! Wait!"" cried Average Pony, as the woodcutter picked her up. But the woodcutter ignored her cries and carried the pony away under his arm.
+Average Pony never finished eating the front door made from jelly babies and Bunny remained trapped in the witch's cage.
+Little Pony stepped up, and approached the house made from chips.
+""I'll eat this whole house,"" said Little Pony. ""Just you watch!""
+Little Pony pulled off a corner of the front door of the house made from chips. He gulped it down smiling, and went back for more.
+And more.
+And more.
+After five or six platefuls, Little Pony started to fidget uncomfortably on the spot.
+He stopped eating chips for a moment, then grabbed another forkful.
+But before he could eat it, there came an almighty roar. A bottom burp louder than a rocket taking off, propelled Little Pony into the sky.
+""Aggghhhhhh!"" cried Little Pony. ""I'm scared of heigh...""
+Little Pony was never seen again.
+Little Pony never finished eating the front door made from chips and Bunny remained trapped in the witch's cage.
+Tiny Pony stepped up, and approached the house made from crisps.
+""I'll eat this whole house,"" said Tiny Pony. ""Just you watch!""
+Tiny Pony pulled off a corner of the front door of the house made from crisps. She gulped it down smiling, and went back for more.
+   And more.
+      And more.
+However, on the next mouthful, the food fell straight out of Tiny Pony's mouth. She tried to stuff in another forkful of crisps, but once again, the food fell out. There just wasn't enough room left in her belly.
+""This is just not fair!"" declared Tiny Pony, and stomped off into the forest.
+Tiny Pony never finished eating the front door made from crisps and Bunny remained trapped in the witch's cage.
+Even-Tinier Pony stepped up, and approached the house made from macarons.
+""I'll eat this whole house,"" said Even-Tinier Pony. ""Just you watch!""
+Even-Tinier Pony pulled off a corner of the front door of the house made from macarons. He gulped it down smiling, and went back for more.
+   And more.
+      And more.
+Suddenly, Even-Tinier Pony stopped eating and started dancing. While he danced, he sang at the top of his lungs, ""Macarons! Watch me eat all the macarons!""
+""It looks as though the macarons are making you hyperactive,"" laughed the witch.
+""Oh no they're not!"" cried Even-Tinier Pony. ""I'm always this excited."" With that, he walked into a tree.
+Bong!
+Even-Tinier Pony banged his head and fell backwards onto his bottom. He passed out, exhausted.
+Even-Tinier Pony never finished eating the front door made from macarons and Bunny remained trapped in the witch's cage.
+""That's it,"" said the witch. ""I win. I get to keep Bunny.""
+""Not so fast,"" said Rainbow. ""There is still one front door to go. The front door of the house made from carrots. And I haven't had a turn yet.
+""I don't have to give you a turn!"" laughed the witch. ""My game. My rules.""
+The woodcutter's voice carried through the forest. ""I think you should give her a chance. It's only fair.""
+""Fine,"" said the witch. ""But you saw what happened to the ponies. She won't last long.""
+""I'll be right back,"" said Rainbow.
+""What?"" said the witch. ""Where's your sense of impatience? I thought you wanted Bunny back.""
+Rainbow ignored the witch and gathered a hefty pile of sticks. She came back to the clearing and started a small camp fire. Carefully, she broke off a piece of the door of the house made from carrots and toasted it over the fire. Once it had cooked and cooled just a little, she took a bite. She quickly devoured the whole piece.
+Rainbow sat down on a nearby log.
+""You fail!"" cackled the witch. ""You were supposed to eat the whole door.""
+""I haven't finished,"" explained Rainbow. ""I am just waiting for my food to go down.""
+When Rainbow's food had digested, she broke off another piece of the door made from carrots. Once more, she toasted her food over the fire and waited for it to cool just a little. She ate it at a leisurely pace then waited for it to digest.
+Eventually, after several sittings, Rainbow was down to the final piece of the door made from carrots. Carefully, she toasted it and allowed it to cool just a little. She finished her final course. Rainbow had eaten the entire front door of the house made from carrots.
+The witch stamped her foot angrily. ""You must have tricked me!"" she said. ""I don't reward cheating!""
+""I don't think so!"" said a voice. It was the woodcutter. He walked back into the clearing, carrying his axe. ""This little girl won fair and square. Now hand over Bunny or I will chop your broomstick in half.""
+The witch looked horrified. She grabbed her broomstick and placed it behind her. Then, huffing, she opened the door of the cage.
+Rainbow hurried over and grabbed Bunny, checking that her favourite toy was all right. Fortunately, Bunny was unharmed.
+Rainbow thanked the woodcutter, grabbed a quick souvenir, and hurried on to meet Twilight. It was starting to get dark.
+When Rainbow got to Twilight's house, her pleasant threw her arms around her.
+""I was so worried!"" cried Twilight. ""You are very late.""
+As Rainbow described her day, she could tell that Twilight didn't believe her. So she grabbed a napkin from her pocket.
+""What's that?"" asked Twilight.
+Rainbow unwrapped a doorknob made from macarons. ""Pudding!"" she said.
+Twilight almost fell off her chair.
+Whose queen is that? I think I know.
+Its owner is quite happy though.
+Full of joy like a vivid rainbow,
+I watch him laugh. I cry hello.
+He gives his queen a shake,
+And laughs until her belly aches.
+The only other sound's the break,
+Of distant waves and birds awake.
+The queen is amusing, dreamy and deep,
+But he has promises to keep,
+After cake and lots of sleep.
+Sweet dreams come to him cheap.
+He rises from his gentle bed,
+With thoughts of kittens in his head,
+He eats his jam with lots of bread.
+Ready for the day ahead.
+With thanks to the poet, Robert Frost, for the underlying structure.
+Please note: This poem was created with our quick poem generator, so results are fairly basic. Our other generators use robots who take your input then trawl the dictionary for rhymes, synonymns and alliteration, creating a more unique experience.
+
+The End";
 
         public static string[] RandomPoemLines => RandomPoem.Split('\n', '\r').ToArray();
     }
